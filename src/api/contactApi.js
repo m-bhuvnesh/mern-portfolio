1c0a738ad9a36e985db417c13a6430f421f0bc9b
bhuvnesh mishra <mbhuvnesh26@gmail.com>
2025-04-24 23:04:17 +0530

    change to live url

--- src/api/contactApi.js
+++ src/api/contactApi.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 
-// Remove trailing slash to avoid accidental "//" in URL
-const Api_Url = "https://mern-portfolio-backend-production.up.railway.app";
+const Api_Url = "mern-portfolio-backend-production-73bf.up.railway.app";
 
 export const sendContactMessage = async (formData) => {
   try {
-    const response = await axios.post(`${Api_Url}/api/contact`, formData);
+    const response = await axios.post(`${Api_Url}/api/contact`, formData, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     return response.data;
   } catch (error) {
     throw error.response?.data || { message: "Unknown error occurred." };
